f5a98b1892b1e376d9c4b123978bc2ca42ec0434
Trenton Le <le.484@buckeyemail.osu.edu>
2022-04-06 19:02:08 -0400

    committing all changes

--- Uncanny.Violin.Api/Program.cs
+++ Uncanny.Violin.Api/Program.cs
@@ -1,4 +1,17 @@
+using Uncanny.Violin.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers(); 
+builder.Services.AddDbContext<StoreContext>(options => 
+	options.UseSqlite("Data Source = ../Registrar.sqlite", 
+	b => b.MigrationsAssembly("Uncanny.Violin.Api"))
+); 
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen(); 
+
 
 // Add services to the container.
 
