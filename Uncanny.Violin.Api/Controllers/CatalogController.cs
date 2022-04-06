91365006485c9d5970cef35dd0751bec8bfdabcb
Trenton Le <le.484@buckeyemail.osu.edu>
2022-04-06 19:22:48 -0400

    POST test

--- Uncanny.Violin.Api/Controllers/CatalogController.cs
+++ Uncanny.Violin.Api/Controllers/CatalogController.cs
@@ -33,7 +33,9 @@ namespace Uncanny.Violin.Api.Controllers
         [HttpPost] 
         public IActionResult Post (Item item) 
         {
-        return Created("/catalog/42", item); 
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return Created($"/catalog/{item.Id}", item);
         }
         
         [HttpPost("{id:int}/ratings")] 
