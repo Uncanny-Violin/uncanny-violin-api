2ed1d9ecd66241c21be7f62149a3a691e1c7767d
Trenton Le <le.484@buckeyemail.osu.edu>
2022-04-26 15:03:32 -0400

    commit all changes
    Co-authored-by: tdue7 <tdue7@users.noreply.github.com>
    Co-authored-by: Eamon McDonough <EamonMcD3@users.noreply.github.com>

--- Uncanny.Violin.Api/Program.cs
+++ Uncanny.Violin.Api/Program.cs
@@ -1,8 +1,18 @@
 using Uncanny.Violin.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Sqlite;
+using Uncanny.Violin.Api.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
+
+string authority = builder.Configuration ["Auth0: Authority"] ?? 
+	throw new ArgumentNullException("Auth0:Authority"); 
+
+string audience = builder.Configuration ["Auth0: Audience"] ?? 
+	throw new ArgumentNullException("Auth0:Audience"); 
+
+builder.Services.AddControllers(); 
 
 builder.Services.AddControllers(); 
 builder.Services.AddDbContext<StoreContext>(options => 
@@ -27,6 +37,25 @@ builder.Services.AddSwaggerGen();
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(options =>
+	{ 
+		options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; 
+		options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; 
+	})
+	.AddJwtBearer(options => 
+{ 
+options.Authority = authority; 
+options.Audience = audience; 
+});
+
+builder.Services.AddAuthorization(options =>
+	{ 
+		options.AddPolicy("delete:catalog", policy => 
+			policy.RequireAuthenticatedUser().RequireClaim("scope", "delete:catalog")); 
+}); 
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -43,6 +72,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
