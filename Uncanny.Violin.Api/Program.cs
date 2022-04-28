d5cd286accddac7f3c19559ad507c8fe92c07e35
Trenton Le <le.484@buckeyemail.osu.edu>
2022-04-28 13:47:57 -0400

    tryin my best

--- Uncanny.Violin.Api/Program.cs
+++ Uncanny.Violin.Api/Program.cs
@@ -1,10 +1,36 @@
 using Uncanny.Violin.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Sqlite;
+using Uncanny.Violin.Api.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers(); 
+string authority = builder.Configuration["Auth0:Authority"] ??
+    throw new ArgumentNullException("Auth0:Authrotiy");
+
+string audience = builder.Configuration["Autho0:Audience"] ??
+    throw new ArgumentException("Auth0:Audience");
+
+builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.Authority = authority;
+        options.Audience = audience;
+    });
+
+builder.Services.AddAuthorization(options =>
+    {
+        options.AddPolicy("delete:catalog", policy =>
+            policy.RequireAuthenticatedUser().RequireClaim("scope", "delete:catalog"));
+    });
+
 builder.Services.AddDbContext<StoreContext>(options => 
 	options.UseSqlite("Data Source = ../Registrar.sqlite", 
 	b => b.MigrationsAssembly("Uncanny.Violin.Api"))
@@ -43,6 +69,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
